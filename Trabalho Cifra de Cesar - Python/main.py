7a9cc2f0030b9b99713f3d17fb4da186cedda834
Eduardo Amaro Maciel <eduardo.maciel@edu.univali.br>
2021-04-07 23:28:33 -0300

    Update main

--- Trabalho Cifra de Cesar - Python/main.py
+++ Trabalho Cifra de Cesar - Python/main.py
@@ -1,8 +1,15 @@
 # ----PROGRAMA PRINCIPAL---- #
 
 import LayoutTrabalho
+import os
+from pyfiglet import figlet_format
 
-LayoutTrabalho.lay()
+os.system("cls")
+result = figlet_format("decifrador", font = "isometric1" )
+print(result\n)
+
+if __name__ == 'main':
+  LayoutTrabalho.lay()
 
 # -> no arquivo uteis é possivel trocar o "dicionariopt.txt" pelo "dicionarioptcompleto", apesar que
 # pode haver queda de performance
